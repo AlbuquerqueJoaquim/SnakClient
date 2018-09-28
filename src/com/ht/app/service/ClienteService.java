4b22ee5303aa45af4729dd4716dab097fddf50fd
Joaquim Albuquerque <joa_kym_junior@hotmail.com>
2018-09-27 20:12:54 -0400

    Criado parte visual do chat, form

---
+++ src/com/ht/app/service/ClienteService.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ht.app.service;
+
+import com.ht.app.bean.ChatMessage;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author joaquimnot
+ */
+public class ClienteService {
+    
+    private Socket socket;
+    private ObjectOutputStream output;
+    
+    //cliente conectando ao servidor
+    public Socket connect(){
+        
+        try {
+            //caso queira conectar a alguma maquina em rede apenas colocar o IP da maquina
+            this.socket = new Socket("localhost", 5555);
+        }  catch (UnknownHostException ex) {
+            Logger.getLogger(ClienteService.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(ClienteService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return socket;
+    }
+    //metodo para envio de mensagem após a conexão com o servidor
+    public void send(ChatMessage message){
+        
+        try {
+            output.writeObject(message);
+        } catch (IOException ex) {
+            Logger.getLogger(ClienteService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+}
